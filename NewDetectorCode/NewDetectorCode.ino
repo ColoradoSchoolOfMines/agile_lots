417fa98e68ad2b50a7e065f1a2627875753ff5fe
Travis Johnson <cjohnso0@mymail.mines.edu>
2015-10-06 19:00:35 -0600

    End of meeting Oct 6

--- NewDetectorCode/NewDetectorCode.ino
+++ NewDetectorCode/NewDetectorCode.ino
@@ -1,78 +1,47 @@
-/*
-Arduino, HMC5883, magnetometer, XBee code 
- by: Roy Stillwell
- Colorado School of Mines
- created on: 3.30.13
- license: This work is licensed under a Creative Commons Attribution license.
- 
- Arduino code example for interfacing with the HMC5883 
- by: Jordan McConnell
- SparkFun Electronics
- created on: 6/30/11
- license: OSHW 1.0, http://freedomdefined.org/OSHW
- 
- Analog input 4 I2C SDA
- Analog input 5 I2C SCL
- 
- EEPROM code adapted from Tomorrow Lab
- Developer: Ted Ullricis_measuringh <ted@tomorrow-lab.com>
- http://tomorrow-lab.com
- 
- */
-
-#include <Wire.h> //I2C Arduino Library
-#include <EEPROM.h>
-#include <Arduino.h>
-#include "EEPROMAnything.h"
-
-
-#define NOTE_C6  1047
-#define NOTE_E6  1319
-#define NOTE_G6  1568
-#define MEM_SIZE 512 //EEPROM memory size (remaining 2 bytes reserved for count)
+ #include <Wire.h> //I2C Arduino Library
 
 #define address 0x1E //0011110b, I2C 7bit address of HMC5883
 
-
+int x,y,z; //triple axis data
 
 void setup(){
   //Initialize Serial and I2C communications
   Serial.begin(9600);
   Wire.begin();
-
+  
   //Put the HMC5883 IC into the correct operating mode
   Wire.beginTransmission(address); //open communication with HMC5883
-  Wire.write(0x02); //select mode register
-  Wire.write(0x00); //continuous measurement mode
-  Wire.endTransmission();
-
-
-  //Tell the HMC5883 where to begin reading data
-  Wire.beginTransmission(address);
-  Wire.write(0x03); //select register 3, X MSB register
+  Wire.send(0x02); //select mode register
+  Wire.send(0x00); //continuous measurement mode
   Wire.endTransmission();
 }
 
 void loop(){
 
-  //Read data from each axis, 2 registers per axis
+  //Tell the HMC5883L where to begin reading data
+  Wire.beginTransmission(address);
+  Wire.send(0x03); //select register 3, X MSB register
+  Wire.endTransmission();
+  
+ 
+ //Read data from each axis, 2 registers per axis
   Wire.requestFrom(address, 6);
   if(6<=Wire.available()){
-    x = Wire.read()<<8; //X msb
-    x |= Wire.read(); //X lsb
-    y = Wire.read()<<8; //Y msb
-    y |= Wire.read(); //Y lsb
-    z = Wire.read()<<8; //Z msb
-    z |= Wire.read(); //Z lsb
+    x = Wire.receive()<<8; //X msb
+    x |= Wire.receive(); //X lsb
+    z = Wire.receive()<<8; //Z msb
+    z |= Wire.receive(); //Z lsb
+    y = Wire.receive()<<8; //Y msb
+    y |= Wire.receive(); //Y lsb
   }
-  delay(40);
+  
+  //Print out values of each axis
+  Serial.print("x: ");
+  Serial.print(x);
+  Serial.print("  y: ");
+  Serial.print(y);
+  Serial.print("  z: ");
+  Serial.println(z);
+  
+  delay(250);
 }
-
-
-
-
-
-
-
-
-
